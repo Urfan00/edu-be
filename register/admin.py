7525a44f290d61e6791e77605a5f79a62131394d
Urfan00 <urfanagazade75@gmail.com>
2024-11-02 15:06:10 +0400

    Register app add import export admins.py

--- register/admin.py
+++ register/admin.py
@@ -1,50 +1,52 @@
 from django.contrib import admin
 from .models import Purpose, SourceOfInformation, University, Filial, Region, Program, Register
+from import_export.admin import ImportExportModelAdmin
+
 
 @admin.register(Purpose)
-class PurposeAdmin(admin.ModelAdmin):
+class PurposeAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(SourceOfInformation)
-class SourceOfInformationAdmin(admin.ModelAdmin):
+class SourceOfInformationAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(University)
-class UniversityAdmin(admin.ModelAdmin):
+class UniversityAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(Filial)
-class FilialAdmin(admin.ModelAdmin):
+class FilialAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(Region)
-class RegionAdmin(admin.ModelAdmin):
+class RegionAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(Program)
-class ProgramAdmin(admin.ModelAdmin):
+class ProgramAdmin(ImportExportModelAdmin):
     list_display = ('id', 'name', 'created_at', 'updated_at')
     search_fields = ('name',)
     list_filter = ('created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
 
 @admin.register(Register)
-class RegisterAdmin(admin.ModelAdmin):
+class RegisterAdmin(ImportExportModelAdmin):
     list_display = ('id', 'first_name' , 'last_name' , 'father_name' , 'email' , 'passport_id' , 'phone_number_1' , 'phone_number_2' , 'gender' , 'lesson_type' , 'status' , 'purpose' , 'source_of_information' , 'university' , 'filial' , 'region' , 'program', 'created_at', 'updated_at')
     search_fields = ('first_name', 'last_name', 'email', 'passport_id')
     list_filter = ('gender' , 'lesson_type' , 'status' , 'purpose' , 'source_of_information' , 'university' , 'filial' , 'region' , 'program', 'created_at', 'updated_at')
